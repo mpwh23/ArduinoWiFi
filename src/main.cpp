69aa862177294be3ca070df6f41096a85f49b545
mpwh23 <melchera23@googlemail.com>
2024-07-16 16:17:13 +0200

    240716

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,12 @@
 // replace
 char clientID[18] = "esp_v1           ";
 
+// networks
+u_int8_t num_nw = sizeof(nw)/sizeof(nw[0]);
+
 // function declarations:
 bool conWiFi();
+int strongest(cred, int);
 
 void setup() {
 
@@ -22,8 +26,22 @@ void setup() {
 
   conWiFi();  
 
-  Serial.println("Setup complete");
-  Serial.println("");
+  Serial.println("Setup done");
+
+    
+    Serial.println("");
+
+    Serial.println();
+
+    
+
+
+    Serial.println("blub");
+ 
+    // Delete the scan result to free memory for code below.
+    WiFi.scanDelete();
+
+
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
@@ -31,65 +49,106 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
+
 // function definitions:
+int strongest(cred lst[], int num){
+  int s = -100;
+  int pos = -1;
+
+  for(u_int8_t j = 0; j < num; j++){
+    if(lst[j].strength != 0 && lst[j].strength > s){
+      s = lst[j].strength;
+      pos = j;
+    }
+  }
+
+  return pos;
+}
+
 bool conWiFi(){
   Serial.println(">");
   Serial.println(">");
-  Serial.print("Connecting to ");
+  //Serial.print("Connecting to ");
+  WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  delay(2000);
+  delay(100);
 
-  WiFi.mode(WIFI_AP_STA);
+  Serial.println("> start scan");
+  // WiFi.scanNetworks will return the number of networks found.
+  int n = WiFi.scanNetworks();
+  Serial.println("> Scan done");
+
   bool con = false;
 
-  WiFi.begin(ssidA, pwA);
+  if (n == 0) {
+        Serial.println(">> no networks found");
+    } else {
+      Serial.print(">> ");
+      Serial.print(n);
+      Serial.println(" networks found");
 
-  Serial.println("  WiFi > Try to connect to default WiFi: ");
-  Serial.print("  WiFi >   " + String(ssidA) + " ");
-  for (int i = 0; i <=15; i++){
-    delay(500);
-    Serial.print(".");
-    if(WiFi.status() == WL_CONNECTED){
-      con = true;
-      Serial.print("  WiFi > Connected to ");
-      Serial.println(ssidA);
-      break;
-    }
-  } 
-  if(!con){
-    Serial.println();
-    
-    WiFi.begin(ssidB, pwB);
-    Serial.println("  WiFi > Try to Connect to alternative WiFi: ");
-    Serial.print("  WiFi >   " + String(ssidB) + " ");
-    for (int i = 0; i <=15; i++){
-      delay(500);
-      Serial.print(".");
-      if(WiFi.status() == WL_CONNECTED){
-        con = true;
-        Serial.print("  WiFi > Connected to ");
-      Serial.println(ssidB);
-        break;
+      int count = 0;
+      
+      // store strength to networks
+      for (int i = 0; i < n; ++i) {
+        for(u_int8_t j = 0; j<(num_nw);j++){
+          if (strcmp(WiFi.SSID(i).c_str(), nw[j].ssid) == 0) {
+            nw[j].strength = WiFi.RSSI(i);
+            count++;
+          }
+        }
+      }
+
+      Serial.print(">> ");
+      Serial.print(count);
+      Serial.println(" networks matching stored credentials");
+
+      // try to connect
+      
+      int sel = strongest(nw, num_nw);
+
+      while(sel > -1){
+        WiFi.begin(nw[sel].ssid, nw[sel].pw);
+        Serial.print("  WiFi > Try to connect to: ");
+        Serial.print(nw[sel].ssid);
+        for (int i = 0; i <=15; i++){
+          delay(500);
+          Serial.print(".");
+          if(WiFi.status() == WL_CONNECTED){
+            con = true;
+            Serial.println();
+            Serial.print("  WiFi > Connected to ");
+            Serial.print(nw[sel].ssid);
+            sel = -1;
+            break;
+          }
+        } 
+        if(!con){
+          // fail to connect, set strength to 0
+          nw[sel].strength = 0;
+          sel = strongest(nw, num_nw);
+          Serial.println();
+        }
       }
     }
-  }
-  Serial.println();
+
+    Serial.println();
   
-  if(con){
-    Serial.print("  WiFi > Station IP Address: ");
-    Serial.println(WiFi.localIP());
+    if(con){
+      Serial.print("  WiFi > Station IP Address: ");
+      Serial.println(WiFi.localIP());
 
-    String s = WiFi.macAddress();
-    s.replace(":", "");
+      String s = WiFi.macAddress();
+      s.replace(":", "");
 
-    s.toCharArray(clientID, 13);
-    Serial.println("ClientID: " + s);
+      s.toCharArray(clientID, 13);
+      Serial.println("  WiFi > ClientID: " + s);
 
-    Serial.println();    
-  }else{
-    Serial.print("  WiFi > ERROR no connection!");
-    Serial.println(); 
-  }
+      Serial.println();    
+    }else{
+      Serial.print("  WiFi > ERROR no connection!");
+      Serial.println(); 
+    }
 
-  return con;
+    return con;
 }
